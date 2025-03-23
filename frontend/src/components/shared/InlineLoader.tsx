60fb7e55d5d728a26d9e8743979f7e405360d53e
Mohan kumar <dmohankumarmech@gmail.com>
2025-03-23 15:56:46 +0530

    UI - Loader and Modal added

---
+++ frontend/src/components/shared/InlineLoader.tsx
@@ -0,0 +1,7 @@
+const InlineLoader = () => {
+  return (
+    <div className="w-6 h-6 border-3 border-white-500 border-t-transparent rounded-full animate-spin"></div>
+  );
+};
+
+export default InlineLoader;
